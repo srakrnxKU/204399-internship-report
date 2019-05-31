9208bcf7b15f019af96867b676b6c99f9edf7bf6
Sirakorn Lamyai <tanersirakorn@gmail.com>
2019-05-31 21:08:06 +0700

    Auto building internship TeX

--- diary/generate_diary.py
+++ diary/generate_diary.py
@@ -2,14 +2,16 @@ import pandas as pd
 import os
 import os.path
 
+this_directory = os.path.dirname(os.path.realpath(__file__))
 file_path = 'images'
 
 entries = pd.read_csv('diary.csv')
 images = os.listdir(file_path)
 entry_format = '''
-\\begin{{figure}}
+\\subsection*{{{caption}}}
+\\begin{{figure}}[h]
     \\centering
-    \\includegraphics[0.8\\textwidth]{{{src}}}
+    \\includegraphics[width=0.5\\textwidth]{{{src}}}
     \\caption{{{caption}}}
 \\end{{figure}}
 
@@ -23,7 +25,8 @@ for index, row in entries.iterrows():
     pretty_date = "{}/{}/{}".format(row['d'], row['m'], row['y']+543)
     matching_files = [i for i in images if date in i]
     if len(matching_files) > 0:
-        full_image_path = os.path.join(file_path, matching_files[0])
+        full_image_path = os.path.join(
+            this_directory, file_path, matching_files[0])
         f.write(entry_format.format(src=full_image_path, caption="ภาพการฝึกงานวันที่ {}".format(
             pretty_date), description=row['description']))
     else:
